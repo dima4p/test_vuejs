da3e1ba956effb9c06b229b2c132d75a05f47e6d
Dmitri Koulikoff <dima@koulikoff.ru>
2019-11-05 22:07:12 +0100

    Button Destroy is added

--- src/components/Companies.vue
+++ src/components/Companies.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container table-responsive">
     <h1>Companies</h1>
-    {{errors[0]}}
 
     <table class="table table-hover table-sm">
       <thead class="thead-dark">
@@ -16,18 +15,15 @@
       </thead>
 
       <tbody>
-
-        <tr v-for="company in companies" v-bind:key="company.id">
-            <td>{{ company.identity }}</td>
-            <td>{{ company.name }}</td>
-            <td><router-link :to="{path: 'employees', query: {company_id: company.id}}">{{ company.employees }}</router-link></td>
-            <td><router-link :to="{path: 'contractors', query: {company_id: company.id}}">{{ company.contractors }}</router-link></td>
-            <td><router-link :to="{path: 'clients', query: {company_id: company.id}}">{{ company.clients }}</router-link></td>
-            <td><router-link :to="`/company/${company.id}`">Show</router-link></td>
-            <td><router-link :to="`/company/${company.id}/edit`">Edit</router-link></td>
-          <!--
-            <td>{{ link_to 'Destroy', company, method: :delete, data: { confirm: 'Are you sure?' } }}</td>
-          -->
+        <tr v-for="(company, index) in companies" v-bind:key="company.id">
+          <td>{{ company.identity }}</td>
+          <td>{{ company.name }}</td>
+          <td><router-link :to="{path: 'employees', query: {company_id: company.id}}">{{ company.employees }}</router-link></td>
+          <td><router-link :to="{path: 'contractors', query: {company_id: company.id}}">{{ company.contractors }}</router-link></td>
+          <td><router-link :to="{path: 'clients', query: {company_id: company.id}}">{{ company.clients }}</router-link></td>
+          <td><router-link :to="`/company/${company.id}`">Show</router-link></td>
+          <td><router-link :to="`/company/${company.id}/edit`">Edit</router-link></td>
+          <td><a href="#" v-on:click="destroy(index)">Destroy</a></td>
         </tr>
       </tbody>
     </table>
@@ -42,7 +38,7 @@ export default {
       companies: [],
       name: [],
       relationship: [],
-      errors: []
+      error: []
     }
   },
 
@@ -58,6 +54,21 @@ export default {
         .catch(e => {
           this.error.push(e)
         })
+    },
+    destroy: function (index) {
+      console.log(this.companies[index].id)
+      if (confirm(`Do you really want to delete the company "${this.companies[index].name}"`)) {
+        this.$root.axios.request({
+          method: 'delete',
+          url: `/companies/${this.companies[index].id}.json`
+        })
+          .then(response => {
+            this.$delete(this.companies, index)
+          })
+          .catch(e => {
+            this.error.push(e)
+          })
+      }
     }
   },
 
